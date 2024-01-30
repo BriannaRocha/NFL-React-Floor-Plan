01f6cf085791b5e9058d4da9e3a9d6c2a6e949da
BriannaRocha <rochabrianna17@gmail.com>
2024-01-29 23:56:22 -0500

    add NFC West component

---
+++ src/NFC_West.jsx
@@ -0,0 +1,15 @@
+import Team from "./Team"
+
+
+const NFC_West = (props) => {
+  return (
+    <div>
+      <Team name={"San Francisco 49ers"} city={"San Francisco"} state={"California"} />
+      <Team name={"Los Angeles Rams"} city={"Los Angeles"} state={"California"} />
+      <Team name={"Seattle Seahawks"} city={"Seattle"} state={"Washington"} />
+      <Team name={"Arizona Cardinals"} city={"Phoenix"} state={"Arizona"} />
+    </div>
+  )
+}
+
+export default NFC_West
