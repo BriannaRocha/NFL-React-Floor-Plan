10ef845c95a029bca4c0f7e1bbeda2093b459f38
BriannaRocha <rochabrianna17@gmail.com>
2024-01-29 22:14:41 -0500

    add NFC_East & North components

---
+++ src/AFC_East.jsx
@@ -0,0 +1,18 @@
+import Bills from "./Bills"
+import Dolphins from "./Dolphins"
+import Jets from "./Jets"
+import Patriots from "./Patriots"
+
+
+const AFC_East = (props) => {
+  return (
+    <div>
+      <Bills />
+      <Dolphins />
+      <Jets />
+      <Patriots />
+    </div>
+  )
+}
+
+export default AFC_East
---
+++ src/AFC_North.jsx
@@ -0,0 +1,18 @@
+import Ravens from "./Ravens"
+import Browns from "./Browns"
+import Steelers from "./Steelers"
+import Bengals from "./Bengals"
+
+
+const AFC_North = (props) => {
+  return (
+    <div>
+      <Ravens />
+      <Browns />
+      <Steelers />
+      <Bengals />
+    </div>
+  )
+}
+
+export default AFC_North
