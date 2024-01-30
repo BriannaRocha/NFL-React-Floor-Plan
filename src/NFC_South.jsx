9aba3c4e361957d0897d58e7e34f285745dac58f
BriannaRocha <rochabrianna17@gmail.com>
2024-01-29 23:51:41 -0500

    add NFC South component

---
+++ src/NFC_South.jsx
@@ -0,0 +1,15 @@
+import Team from "./Team"
+
+
+const NFC_South = (props) => {
+  return (
+    <div>
+      <Team name={"Tampa Bay Buccaneers"} city={"Tampa Bay"} state={"Florida"} />
+      <Team name={"New Orleans Saints"} city={"New Orleans"} state={"Louisiana"} />
+      <Team name={"Atlanta Falcons"} city={"Atlanta Falcons"} state={"Georgia"} />
+      <Team name={"Carolina Panthers"} city={"Charlotte"} state={"North Carolina"} />
+    </div>
+  )
+}
+
+export default NFC_South
